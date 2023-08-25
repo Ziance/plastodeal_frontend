d668f2a92aa89528d621c602974ae135d1709070
Ashish6896 <ashish.gajjar@ziancetechnologies.com>
2023-08-25 13:00:25 +0530

    CSS changes

--- src/Pages/Faq/index.tsx
+++ src/Pages/Faq/index.tsx
@@ -5,15 +5,16 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import WrapperComponent from "../../components/WrapperComponent";
-import { useTranslation} from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
 
 export default function BasicAccordion() {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
   return (
     <WrapperComponent isHeader>
-      <div>
-        <Typography textAlign="left" width="95%" fontSize="30px">{t('Faq.heading')}</Typography>
+      <div style={{ padding: "40px" }}>
+        <Typography textAlign="left" width="95%" fontSize="30px">
+          {t("Faq.heading")}
+        </Typography>
         <br />
         <Accordion>
           <AccordionSummary
@@ -21,11 +22,11 @@ export default function BasicAccordion() {
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography>{t('Faq.question1')}</Typography>
+            <Typography>{t("Faq.question1")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-            {t('Faq.answer1')}
+              {t("Faq.answer1")}
               {/* It is simply a platform for the plastic allied industries. Plastic
               allied industries are able to exhibit their products. It is just a
               B2B platform to provide and exhibit themselves. It is a subscription
@@ -40,11 +41,11 @@ export default function BasicAccordion() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography>{t('Faq.question2')}</Typography>
+            <Typography>{t("Faq.question2")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-            {t('Faq.answer2')}
+              {t("Faq.answer2")}
               {/* Step 1: Go to login page Step 2: Click on sign up Step 3: Free login
               1) Enter personal information and create login step 4: Company login
               1) Enter personal information 2) Enter company information 3) Pay
@@ -59,12 +60,10 @@ export default function BasicAccordion() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography>{t('Faq.question3')}</Typography>
+            <Typography>{t("Faq.question3")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-            {t('Faq.answer3')}
-            </Typography>
+            <Typography>{t("Faq.answer3")}</Typography>
           </AccordionDetails>
         </Accordion>
         <Accordion>
@@ -73,12 +72,10 @@ export default function BasicAccordion() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography>{t('Faq.question4')}</Typography>
+            <Typography>{t("Faq.question4")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-            {t('Faq.answer4')}
-            </Typography>
+            <Typography>{t("Faq.answer4")}</Typography>
           </AccordionDetails>
         </Accordion>
         <Accordion>
@@ -87,12 +84,10 @@ export default function BasicAccordion() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography>{t('Faq.question5')}</Typography>
+            <Typography>{t("Faq.question5")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-            {t('Faq.answer5')}
-            </Typography>
+            <Typography>{t("Faq.answer5")}</Typography>
           </AccordionDetails>
         </Accordion>
 
@@ -102,12 +97,10 @@ export default function BasicAccordion() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography>{t('Faq.question6')}</Typography>
+            <Typography>{t("Faq.question6")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-            {t('Faq.answer6')}
-            </Typography>
+            <Typography>{t("Faq.answer6")}</Typography>
           </AccordionDetails>
         </Accordion>
         <Accordion>
@@ -116,16 +109,13 @@ export default function BasicAccordion() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography>{t('Faq.question7')}</Typography>
+            <Typography>{t("Faq.question7")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-            {t('Faq.answer7')}
-            </Typography>
+            <Typography>{t("Faq.answer7")}</Typography>
           </AccordionDetails>
         </Accordion>
       </div>
     </WrapperComponent>
-
   );
 }
